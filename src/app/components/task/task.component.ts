46e7eb16e0a8a3f0ca2a72d326488c944c983a96
dmytro-poputnikov <dpoputnikov@gmail.com>
2023-09-30 23:00:19 +0200

    change task background to red

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -3,8 +3,47 @@ import { Task } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css'],
+  template: `
+    <div class="list-item {{ task?.state }}">
+      <label
+        [attr.aria-label]="'archiveTask-' + task?.id"
+        for="checked-{{ task?.id }}"
+        class="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled="true"
+          [defaultChecked]="task?.state === 'TASK_ARCHIVED'"
+          name="checked-{{ task?.id }}"
+          id="checked-{{ task?.id }}"
+        />
+        <span class="checkbox-custom" (click)="onArchive(task?.id)"></span>
+      </label>
+      <label
+        [attr.aria-label]="task?.title + ''"
+        for="title-{{ task?.id }}"
+        class="title"
+      >
+        <input
+          type="text"
+          [value]="task?.title"
+          readonly="true"
+          id="title-{{ task?.id }}"
+          name="title-{{ task?.id }}"
+          placeholder="Input title"
+          style="background: red;"
+        />
+      </label>
+      <button
+        *ngIf="task?.state !== 'TASK_ARCHIVED'"
+        class="pin-button"
+        [attr.aria-label]="'pinTask-' + task?.id"
+        (click)="onPin(task?.id)"
+      >
+        <span class="icon-star"></span>
+      </button>
+    </div>
+  `,
 })
 export class TaskComponent {
   /**
