2e4e9082526af72703388995d4577fed28370042
dmytro-poputnikov <dpoputnikov@gmail.com>
2023-09-30 22:26:09 +0200

    Add screen and tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { NgxsModule } from '@ngxs/store';
-import { environment } from 'src/environments/environment.prod';
-import { TaskModule } from './components/task.module';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+
+import InboxScreenComponent from './components/inbox-screen.component';
+import PureInboxScreenComponent from './components/pure-inbox-screen.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InboxScreenComponent, PureInboxScreenComponent],
   imports: [
     BrowserModule,
     TaskModule,
-    NgxsModule.forRoot([], {
-      developmentMode: !environment.production,
-    }),
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
     NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot({
-      disabled: environment.production,
-    }),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
